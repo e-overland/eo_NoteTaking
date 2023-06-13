60c7176ed6f7ce80eb298de58696d217f84c85be
Ethan Overland <overlande@mvfcu.coop>
2023-06-13 10:53:03 -0800

    Add project files.

---
+++ frmMain.Designer.cs
@@ -0,0 +1,193 @@
+﻿
+namespace eo_NoteTaking
+{
+    partial class frmMain
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(frmMain));
+            this.txtTitle = new System.Windows.Forms.TextBox();
+            this.lblTitle = new System.Windows.Forms.Label();
+            this.lblMessage = new System.Windows.Forms.Label();
+            this.lblMessageList = new System.Windows.Forms.Label();
+            this.btnNew = new System.Windows.Forms.Button();
+            this.btnSave = new System.Windows.Forms.Button();
+            this.btnRead = new System.Windows.Forms.Button();
+            this.btnDelete = new System.Windows.Forms.Button();
+            this.txtMessage = new System.Windows.Forms.TextBox();
+            this.gridvwMessageList = new System.Windows.Forms.DataGridView();
+            ((System.ComponentModel.ISupportInitialize)(this.gridvwMessageList)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // txtTitle
+            // 
+            this.txtTitle.Location = new System.Drawing.Point(63, 64);
+            this.txtTitle.Name = "txtTitle";
+            this.txtTitle.Size = new System.Drawing.Size(281, 22);
+            this.txtTitle.TabIndex = 0;
+            // 
+            // lblTitle
+            // 
+            this.lblTitle.AutoSize = true;
+            this.lblTitle.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblTitle.Location = new System.Drawing.Point(59, 41);
+            this.lblTitle.Name = "lblTitle";
+            this.lblTitle.Size = new System.Drawing.Size(46, 20);
+            this.lblTitle.TabIndex = 1;
+            this.lblTitle.Text = "Title:";
+            // 
+            // lblMessage
+            // 
+            this.lblMessage.AutoSize = true;
+            this.lblMessage.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F);
+            this.lblMessage.Location = new System.Drawing.Point(63, 98);
+            this.lblMessage.Name = "lblMessage";
+            this.lblMessage.Size = new System.Drawing.Size(82, 20);
+            this.lblMessage.TabIndex = 2;
+            this.lblMessage.Text = "Message:";
+            // 
+            // lblMessageList
+            // 
+            this.lblMessageList.AutoSize = true;
+            this.lblMessageList.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F);
+            this.lblMessageList.Location = new System.Drawing.Point(410, 41);
+            this.lblMessageList.Name = "lblMessageList";
+            this.lblMessageList.Size = new System.Drawing.Size(115, 20);
+            this.lblMessageList.TabIndex = 4;
+            this.lblMessageList.Text = "Message List:";
+            // 
+            // btnNew
+            // 
+            this.btnNew.Location = new System.Drawing.Point(63, 390);
+            this.btnNew.Name = "btnNew";
+            this.btnNew.Size = new System.Drawing.Size(62, 28);
+            this.btnNew.TabIndex = 6;
+            this.btnNew.Text = "New";
+            this.btnNew.UseVisualStyleBackColor = true;
+            this.btnNew.Click += new System.EventHandler(this.btnNew_Click);
+            // 
+            // btnSave
+            // 
+            this.btnSave.Location = new System.Drawing.Point(133, 390);
+            this.btnSave.Name = "btnSave";
+            this.btnSave.Size = new System.Drawing.Size(62, 28);
+            this.btnSave.TabIndex = 7;
+            this.btnSave.Text = "Save";
+            this.btnSave.UseVisualStyleBackColor = true;
+            this.btnSave.Click += new System.EventHandler(this.btnSave_Click);
+            // 
+            // btnRead
+            // 
+            this.btnRead.Location = new System.Drawing.Point(414, 390);
+            this.btnRead.Name = "btnRead";
+            this.btnRead.Size = new System.Drawing.Size(62, 28);
+            this.btnRead.TabIndex = 8;
+            this.btnRead.Text = "Read";
+            this.btnRead.UseVisualStyleBackColor = true;
+            this.btnRead.Click += new System.EventHandler(this.btnRead_Click);
+            // 
+            // btnDelete
+            // 
+            this.btnDelete.Location = new System.Drawing.Point(482, 390);
+            this.btnDelete.Name = "btnDelete";
+            this.btnDelete.Size = new System.Drawing.Size(62, 28);
+            this.btnDelete.TabIndex = 9;
+            this.btnDelete.Text = "Delete";
+            this.btnDelete.UseVisualStyleBackColor = true;
+            this.btnDelete.Click += new System.EventHandler(this.btnDelete_Click);
+            // 
+            // txtMessage
+            // 
+            this.txtMessage.Location = new System.Drawing.Point(63, 122);
+            this.txtMessage.Multiline = true;
+            this.txtMessage.Name = "txtMessage";
+            this.txtMessage.Size = new System.Drawing.Size(281, 252);
+            this.txtMessage.TabIndex = 10;
+            // 
+            // gridvwMessageList
+            // 
+            this.gridvwMessageList.AllowUserToAddRows = false;
+            this.gridvwMessageList.AllowUserToDeleteRows = false;
+            this.gridvwMessageList.AllowUserToOrderColumns = true;
+            this.gridvwMessageList.AllowUserToResizeColumns = false;
+            this.gridvwMessageList.AllowUserToResizeRows = false;
+            this.gridvwMessageList.BackgroundColor = System.Drawing.SystemColors.Window;
+            this.gridvwMessageList.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.gridvwMessageList.ColumnHeadersVisible = false;
+            this.gridvwMessageList.Location = new System.Drawing.Point(414, 64);
+            this.gridvwMessageList.MultiSelect = false;
+            this.gridvwMessageList.Name = "gridvwMessageList";
+            this.gridvwMessageList.ReadOnly = true;
+            this.gridvwMessageList.RowHeadersVisible = false;
+            this.gridvwMessageList.RowHeadersWidth = 51;
+            this.gridvwMessageList.RowTemplate.Height = 24;
+            this.gridvwMessageList.ScrollBars = System.Windows.Forms.ScrollBars.None;
+            this.gridvwMessageList.Size = new System.Drawing.Size(240, 310);
+            this.gridvwMessageList.TabIndex = 11;
+            // 
+            // frmMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(676, 430);
+            this.Controls.Add(this.gridvwMessageList);
+            this.Controls.Add(this.txtMessage);
+            this.Controls.Add(this.btnDelete);
+            this.Controls.Add(this.btnRead);
+            this.Controls.Add(this.btnSave);
+            this.Controls.Add(this.btnNew);
+            this.Controls.Add(this.lblMessageList);
+            this.Controls.Add(this.lblMessage);
+            this.Controls.Add(this.lblTitle);
+            this.Controls.Add(this.txtTitle);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.Fixed3D;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "frmMain";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Ethan\'s Note Taking App";
+            this.Load += new System.EventHandler(this.frmMain_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.gridvwMessageList)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtTitle;
+        private System.Windows.Forms.Label lblTitle;
+        private System.Windows.Forms.Label lblMessage;
+        private System.Windows.Forms.Label lblMessageList;
+        private System.Windows.Forms.Button btnNew;
+        private System.Windows.Forms.Button btnSave;
+        private System.Windows.Forms.Button btnRead;
+        private System.Windows.Forms.Button btnDelete;
+        private System.Windows.Forms.TextBox txtMessage;
+        private System.Windows.Forms.DataGridView gridvwMessageList;
+    }
+}
+
---
+++ frmMain.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace eo_NoteTaking
+{
+    public partial class frmMain : Form
+    {
+        DataTable c_dtNoteTable;
+
+        public frmMain()
+        {
+            InitializeComponent();
+        }
+
+        /// <summary>
+        /// On load create the table and the columns for the grid view.
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void frmMain_Load(object sender, EventArgs e)
+        {
+            c_dtNoteTable = new DataTable();
+
+            c_dtNoteTable.Columns.Add("Title", typeof(String));
+            c_dtNoteTable.Columns.Add("Message", typeof(String));
+
+            gridvwMessageList.DataSource = c_dtNoteTable;
+
+            gridvwMessageList.Columns["Message"].Visible = false;
+            gridvwMessageList.Columns["Title"].Width = 240;
+        }
+
+        /// <summary>
+        /// Clear the text fields after new has been clicked.
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void btnNew_Click(object sender, EventArgs e)
+        {
+            txtTitle.Clear();
+            txtMessage.Clear();
+        }
+
+        /// <summary>
+        /// Add the row after save is clicked.
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void btnSave_Click(object sender, EventArgs e)
+        {
+            c_dtNoteTable.Rows.Add(txtTitle.Text, txtMessage.Text);
+        }
+
+        /// <summary>
+        /// Grab the current row and display the title and message.
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void btnRead_Click(object sender, EventArgs e)
+        {
+            // Grabbing the current row that is selected.
+            int l_intRowIndex = gridvwMessageList.CurrentCell.RowIndex;
+            
+            // If a row is selected the index can't be -1.
+            if (l_intRowIndex > -1)
+            {
+                // Set the text fields to the stored note they wish to view.
+                txtTitle.Text = c_dtNoteTable.Rows[l_intRowIndex]["Title"].ToString();
+                txtMessage.Text = c_dtNoteTable.Rows[l_intRowIndex]["Message"].ToString();
+            }
+        }
+
+        /// <summary>
+        /// Delete the selected row from the table.
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void btnDelete_Click(object sender, EventArgs e)
+        {
+            int l_intRowIndex = -1;
+
+            // Avoid Delete functionality when there aren't any rows in the table.
+            if (gridvwMessageList.CurrentCell != null)
+            {
+                l_intRowIndex = gridvwMessageList.CurrentCell.RowIndex;
+            }
+
+            // If a row is selected the index can't be -1.
+            if (l_intRowIndex > -1)
+            {
+                c_dtNoteTable.Rows[l_intRowIndex].Delete();
+            }
+        }
+    }
+}
